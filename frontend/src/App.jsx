550805f7d5260a498710958f3bea5cac2ca91ffd
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-01-18 13:59:39 +0300

    frontend - products components created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
 import Header from './components/Layout/Layouts/Header/Header'
 import Sliders from './components/Layout/Sliders/Sliders'
 import Footer from './components/Layout/Layouts/Footer/Footer'
 import Policy from './components/Layout/Layouts/Policy/Policy'
 import Categories from './components/Layout/Categories/Categories'
+import Products from './components/Layout/Products/Products'
+import './App.css'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Sliders />
       <Categories />
+      <Products />
       <Policy />
       <Footer />
     </div>
