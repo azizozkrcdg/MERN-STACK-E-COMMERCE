e339a043a48375d8e6d8cb4735effaa0598c4bde
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-01-18 13:25:56 +0300

    Categories components created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Layout/Layouts/Header/Header'
 import Sliders from './components/Layout/Sliders/Sliders'
 import Footer from './components/Layout/Layouts/Footer/Footer'
 import Policy from './components/Layout/Layouts/Policy/Policy'
+import Categories from './components/Layout/Categories/Categories'
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <div>
       <Header />
       <Sliders />
-      <h1>HELLO</h1>
+      <Categories />
       <Policy />
       <Footer />
     </div>
