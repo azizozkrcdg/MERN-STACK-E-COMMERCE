98da6414cf6368aabd4dc9de4b54c5f504f20be6
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-01-18 13:05:53 +0300

    Frontend-components created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './components/Layout/Layouts/Header/Header'
+import Sliders from './components/Layout/Sliders/Sliders'
+import Footer from './components/Layout/Layouts/Footer/Footer'
+import Policy from './components/Layout/Layouts/Policy/Policy'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Sliders />
+      <h1>HELLO</h1>
+      <Policy />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
