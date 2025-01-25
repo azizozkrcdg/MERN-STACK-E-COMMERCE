d39b4871ba97a06e33d57aba7e189fad87624a8a
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-01-25 15:46:28 +0300

    Frontend - Modals dialog copmonents created

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import Footer from "../components/Layout/Layouts/Footer/Footer"
 import Header from "../components/Layout/Layouts/Header/Header"
 import PropTypes from "prop-types";
-import Search from "../components/Layout/Modals/Search";
+import Search from "../components/Layout/Modals/Search/Search";
+import Dialog from "../components/Layout/Modals/Dialog/Dialog";
 
 const MainLayout = ({children}) => {
   const [isSearchShow, setIsSearchShow] = useState(false);
+  const [isDialogShow, setIsDialogShow] = useState(true);
+
   return (
     <div className="main-layout">
+        <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow}/>
         <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow}/>
         <Header setIsSearchShow={setIsSearchShow} />
         {children}
