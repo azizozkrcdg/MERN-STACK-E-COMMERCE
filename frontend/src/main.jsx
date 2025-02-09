095df69c573c40b00a29831c202854870e45947d
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-02-09 13:33:20 +0300

    Frontend - slick slider has been created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import MainLayout from './Layouts/MainLayout.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import MainLayout from "./Layouts/MainLayout.jsx";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MainLayout>
       <App />
     </MainLayout>
-    
-  </StrictMode>,
-)
+  </StrictMode>
+);
