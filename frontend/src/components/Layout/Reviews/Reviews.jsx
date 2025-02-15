6c1f7664aa18a66767b1fa791bbf6c33f1caa0b7
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-02-15 14:23:29 +0300

    Frontend - useState is used for tab switching

--- frontend/src/components/Layout/Reviews/Reviews.jsx
+++ frontend/src/components/Layout/Reviews/Reviews.jsx
@@ -1,10 +1,11 @@
-import "./Reviews.css";
 import ReviewsForm from "./ReviewsForm";
 import ReviewsItem from "./ReviewsItem";
+import PropTypes from "prop-types";
+import "./Reviews.css";
 
-const Reviews = () => {
+const Reviews = ({active}) => {
   return (
-    <div className="tab-panel-reviews">
+    <div className={`tab-panel-reviews ${active}`}>
       <h3>2 reviews for Basic Colored Sweatpants With Elastic Hems</h3>
       <div className="comments">
         <ol className="comment-list">
@@ -19,6 +20,10 @@ const Reviews = () => {
       {/* <!-- comment form end --> */}
     </div>
   );
+};
+
+Reviews.propTypes = {
+  active : PropTypes.string
 };
 
 export default Reviews;
