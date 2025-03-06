88caec124d454bedcbbf68fffbb2d2a054b3c0b5
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-03-06 19:04:29 +0300

    Backend - Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+const connection = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://azz:BVvsDEucfMqAqVdu@e-commerce.yzmra.mongodb.net/");
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+}
 
 app.get("/", (req, res) => {
   res.send("homepage");
@@ -8,5 +18,6 @@ app.get("/", (req, res) => {
 
 const port = 5000;
 app.listen(port, () => {
+    connection();
   console.log(`server is running on port ${port}`);
 });
