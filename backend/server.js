a5f2bcb1948eb747ecd2f9a40725d7d7262cb7c3
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-03-06 18:52:13 +0300

    Backend - express and nodemon installed

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 
 const app = express();
 
-const port = 5000;
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
 
+const port = 5000;
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
