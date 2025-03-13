03a916e99981ce10655d4c09a2fc0820127dc3d1
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-03-13 19:30:33 +0300

    Backend - Category and Product models created

---
+++ backend/models/Category.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const CategorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+  { Timestamps: true }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+
+export default Category;
