ce467cf8959dd4a221f3d15d211c14e9919d9630
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-03-21 21:47:08 +0300

    Backend - brcrypt used

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -6,12 +7,18 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password, role, avatar } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Böyle bir kullanıcı zaten var" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       role,
-      avatar
+      avatar,
     });
     await newUser.save();
 
