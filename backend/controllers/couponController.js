39fb1667896f324c7a4e07c42234e28c75ec49ef
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2025-04-06 14:24:02 +0300

    Backend - getCoupon, getAllCoupons, updateCoupon, deleteCoupon controllers created

--- backend/controllers/couponController.js
+++ backend/controllers/couponController.js
@@ -11,5 +11,68 @@ const createCoupon = async (req, res) => {
   }
 };
 
-const couponController = {createCoupon};
+const getCoupon = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const coupon = await Coupon.findById(couponId);
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Kupon bulunamadı!" });
+    }
+
+    res.status(200).json(coupon);
+  } catch (error) {
+    res.status(500).json({ error: "Server error!" });
+  }
+};
+
+const getAllCoupons = async (req, res) => {
+  const coupons = await Coupon.find();
+
+  if (!coupons) {
+    return res.status(404).json({ error: "Kupon bulunamadı!" });
+  }
+
+  res.status(200).json(coupons);
+};
+
+const updateCoupon = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const updates = req.body;
+    const coupon = await Coupon.findById(couponId);
+
+    if (!coupon) {
+      return res.status(404).json({ error: "Kupon bulunamadı!" });
+    }
+
+    const updatedCoupon = await Coupon.findByIdAndUpdate(couponId, updates, {
+      new: true,
+    });
+
+    res.status(200).json(updatedCoupon);
+  } catch (error) {
+    res.status(404).json({ error: "Server error" });
+  }
+};
+
+const deleteCoupon = async (req, res) => {
+  const couponId = req.params.couponId;
+  const coupon = await Coupon.findById(couponId);
+
+  if (!coupon) {
+    return res.status(404).json({ error: "Kupon bulunamadı!" });
+  }
+
+  const deletedCoupon = await Coupon.findByIdAndDelete(coupon);
+  res.status(200).json(deletedCoupon);
+}
+
+const couponController = {
+  createCoupon,
+  getCoupon,
+  getAllCoupons,
+  updateCoupon,
+  deleteCoupon
+};
 export default couponController;
--- backend/routes/coupon.js
+++ backend/routes/coupon.js
@@ -4,5 +4,9 @@ import couponController from "../controllers/couponController.js";
 const router = express.Router();
 
 router.route("/").post(couponController.createCoupon);
+router.route("/:couponId").get(couponController.getCoupon);
+router.route("/").get(couponController.getAllCoupons);
+router.route("/:couponId").put(couponController.updateCoupon);
+router.route("/:couponId").delete(couponController.deleteCoupon);
 
 export default router;
